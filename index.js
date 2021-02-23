29c50820cc0cecf84e70f7d9be33a119b06d3c95
Marina Oliveira <marinaoliveira.r07@gmail.com>
2021-02-23 15:50:11 -0300

    fix: port issues

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const cors = require('cors')
 const router = require('./src/routes')
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT
 const app = express()
 
 app.use(bodyparser.json({ extended: true }))
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(router)
-app.listen(PORT, '0.0.0.0', null, () => console.log('server running'))
+app.listen(PORT || 8081, '0.0.0.0', null, () => console.log('server running'))
