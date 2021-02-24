42fc0fc8d23cb56874e61e5c6ce31180d703293a
Marina Oliveira <marinaoliveira.r07@gmail.com>
2021-02-24 09:57:31 -0300

    fix: database connection

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,12 +2,12 @@ const { Client } = require('pg')
 require('dotenv').config()
 
 const client = new Client({
-  connectionString: process.env.CONNECTION_STRING,
-  //   host: process.env.DB_HOST,
-  //   port: 5432,
-  //   password: process.env.DB_PW,
-  //   user: process.env.DB_USER,
-  //   database: process.env.DB_NAME,
+  //   connectionString: process.env.CONNECTION_STRING,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PW,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false
   }
